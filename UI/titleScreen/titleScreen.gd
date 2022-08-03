504901b93860bc7203d42a33a85eaf10a9999fdc
Krzystof Staroń <kisiel3141@gmail.com>
2022-08-03 08:46:29 +0200

    TitleScreen is working

---
+++ UI/titleScreen/titleScreen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+export var gameScene : PackedScene
+export var settingScene : PackedScene
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to(gameScene)
+
+func _on_settings_pressed() -> void:
+	pass
